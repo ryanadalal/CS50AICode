c8f924b1493e180321459ef6cb4ed2147b216057
Ryan Dalal <ryanadalal@gmail.com>
2024-10-20 11:22:39 +0700

    final model

--- traffic.py
+++ traffic.py
@@ -58,15 +58,24 @@ def load_data(data_dir):
     be a list of integer labels, representing the categories for each of the
     corresponding `images`.
     """
+
+    print("Loading images from data directory")
+
     images = []
     labels = []
 
     for i in range(0, NUM_CATEGORIES):
         for imgfile in os.listdir(os.path.join(data_dir, str(i))):
             img = cv2.imread(os.path.join(data_dir, str(i), imgfile), )
-            img.resize(IMG_WIDTH, IMG_HEIGHT, 3)
+            img = cv2.resize(img, (IMG_WIDTH, IMG_HEIGHT))
+            img = img/255
             images.append(img)
             labels.append(i)
+
+    if len(labels) != len(images):
+        sys.exit("Error mismatched label and image sizes")
+    else:
+        print(f'Successfully loaded {len(labels)} images and labels')
 
     return (images, labels)
 
@@ -77,7 +86,6 @@ def get_model():
     `input_shape` of the first layer is `(IMG_WIDTH, IMG_HEIGHT, 3)`.
     The output layer should have `NUM_CATEGORIES` units, one for each category.
     """
-
     model = tf.keras.models.Sequential([
 
         # Convolutional layer. Learn 32 filters using a 3x3 kernel
@@ -105,7 +113,7 @@ def get_model():
         # Add an output layer with output units for all 10 digits
         tf.keras.layers.Dense(NUM_CATEGORIES, activation="softmax")
     ])
-    print(model.summary())
+    model.summary()
     model.compile(
         optimizer="adam",
         loss="categorical_crossentropy",
